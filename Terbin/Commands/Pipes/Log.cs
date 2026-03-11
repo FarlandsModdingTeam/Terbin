512a80f436433c865039ef138ad4a292e894acc8
PabloGHub <149364828+PabloGHub@users.noreply.github.com>
2026-03-11 14:55:35 +0100

    create LoggerPipe and Log

---
+++ Terbin/Commands/Pipes/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terbin.Pipes
+{
+    public struct Log
+    {
+        private LogLevel _level;
+        private string _message;
+
+
+        public LogLevel Level
+        {
+            get { return _level; }
+            set { _level = value; }
+        }
+        public string Messager
+        {
+            get { return _message; }
+            set { _message = value; }
+        }
+
+
+        public Log(string eMesager)
+        {
+            this._level = LogLevel.Info;
+            this._message = eMesager;
+        }
+        public Log(LogLevel eLevel, string eMesager)
+        {
+            this._level = eLevel;
+            this._message = eMesager;
+        }
+    }
+}
---
+++ Terbin/Commands/Pipes/LoggerPipe.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terbin.Pipes
+{
+
+
+
+    public class LoggerPipe
+    {
+        private readonly object _lock = new();
+
+
+        public LoggerPipe()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+
+        private async Task communicateLog()
+        {
+
+        }
+
+        public void Log(LogLevel level, string message)
+        {
+            lock (_lock)
+            {
+                // TODO: Determinar escribir
+            }
+        }
+    }
+}
